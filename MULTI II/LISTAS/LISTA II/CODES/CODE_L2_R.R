c1a3cf6b8b62ddbd589b5bdd9f1c0f1be5254910
Breno C R Silva <122389486+csilv7@users.noreply.github.com>
2025-07-16 17:34:51 -0300

    Add files via upload

--- MULTI II/LISTAS/LISTA II/CODES/CODE_L2_R.R
+++ MULTI II/LISTAS/LISTA II/CODES/CODE_L2_R.R
@@ -2,10 +2,15 @@
 # [*] CONFIGURAÇÕES INICIAIS
 # --------------------------
 library(readxl)
-library(tidyverse)
-library(MVN)
+
+library(dplyr)
+
 library(ggplot2)
-library(car)
+
+library(MVN)
+library(biotools)
+
+library(rstatix)
 
 # Caminho do arquivo.XLSX
 path <- "~/PROJETOS/VS Code/MULTIVARIADA - VS/MULTI II/LISTAS/LISTA II/DATASETS/DADOS_LISTA_II.xlsx"
@@ -17,7 +22,9 @@ DADOS_LISTA_II <- read_excel(path, sheet = "PEIXES")
 # [1] CONVERTA O BANCO DE DADOS PARA O FORMATO LONGO
 # --------------------------------------------------
 
-# 1. Empilhar os dados por método
+# ----------------------------
+# [1.1] OBTER DADOS POR MÉTODO
+# ----------------------------
 m1 <- DADOS_LISTA_II %>%
   select(starts_with("M1")) %>%
   rename(AROMA = M1y1, SABOR = M1y2, TEXTURA = M1y3, UMIDADE = M1y4) %>%
@@ -33,22 +40,31 @@ m3 <- DADOS_LISTA_II %>%
   rename(AROMA = M3y1, SABOR = M3y2, TEXTURA = M3y3, UMIDADE = M3y4) %>%
   mutate(Method = "M3")
 
-# 2. Juntar tudo em um único data frame
-df <- bind_rows(m1, m2, m3) %>%
+# --------------------------------------
+# [1.2] CONCATENAR E CONVERTER PARA LONG
+# --------------------------------------
+df.LONG <- bind_rows(m1, m2, m3) %>%
   mutate(Method = factor(Method)) %>%
   pivot_longer(cols = AROMA:UMIDADE, names_to = "VARIABLE", values_to = "SCORE")
+
+# Salvar Dados em Formato Long
+write.csv(df.LONG, file = "~/PROJETOS/VS Code/MULTIVARIADA - VS/MULTI II/LISTAS/LISTA II/DATASETS/DADOS_L2Q2_LONG.csv")
 
 # -------------------------------------------
 # [2] FAÇA UMA ANÁLISE EXPLORATÓRIA DOS DADOS
 # -------------------------------------------
 
-# Medidas de Resumo
-summary(df %>% filter(Method == "M1") %>% select(starts_with("y"))) # Método 1
-summary(df %>% filter(Method == "M2") %>% select(starts_with("y"))) # Método 2
-summary(df %>% filter(Method == "M3") %>% select(starts_with("y"))) # Método 3
+# -----------------------
+# [2.1] MEDIDAS DE RESUMO
+# -----------------------
+summary(m1) # Método 1
+summary(m2) # Método 2
+summary(m3) # Método 3
 
-# Boxplot
-ggplot(data = df, aes(x = VARIABLE, y = SCORE, color = Method)) +
+# -------------
+# [2.2] BOXPLOT
+# -------------
+ggplot(data = df.LONG, aes(x = VARIABLE, y = SCORE, color = Method)) +
   geom_boxplot() +
   labs(x = "Características", y = "Score", color = "Método") +
   theme(
@@ -57,8 +73,17 @@ ggplot(data = df, aes(x = VARIABLE, y = SCORE, color = Method)) +
   ) +
   theme_classic(base_size = 12)
 
-# Visualizações variadas
+# -----------------------------
+# [2.3] MÚLTIPLAS VISUALIZAÇÕES
+# -----------------------------
+
+# Converter para o Formato Wide
 df.WIDE <- bind_rows(m1, m2, m3) %>% mutate(Method = factor(Method))
+
+# Salvar Dados em Formato Wide
+write.csv(df.WIDE, file = "~/PROJETOS/VS Code/MULTIVARIADA - VS/MULTI II/LISTAS/LISTA II/DATASETS/DADOS_L2Q2_WIDE.csv")
+
+# Gerar Visualização
 GGally::ggpairs(df.WIDE, 
                 mapping = aes(colour = Method, alpha = 0.75), 
                 columns = colnames(df.WIDE)[1:4]) +
@@ -67,3 +92,68 @@ GGally::ggpairs(df.WIDE,
 # --------------------------------------
 # [3] FAÇA A VERIFICAÇÃO DE PRESSUPOSTOS
 # --------------------------------------
+
+# ------------------------------
+# [3.1] NORMALIDADE MULTIVARIADA
+# ------------------------------
+
+# Teste de Mardia
+mardia.test <- mvn(df.WIDE[, -5], mvnTest = "mardia") ; mardia.test
+
+# Teste deHenze-Zirklers
+hz.test <- mvn(df.WIDE[, -5], mvnTest = "hz") ; hz.test
+
+# Teste de Royston
+royston.test <- mvn(df.WIDE[, -5], mvnTest = "royston") ; royston.test
+
+# ------------------------------------------------
+# [3.2] HOMOCEDASTICIDADE DE MATRIZ DE COVARIÂNCIA
+# ------------------------------------------------
+
+# Teste M de Box
+boxM.test <- boxM(data = df.WIDE[, -5], grouping = df.WIDE$Method) ; boxM.test
+
+# --------------------------------------------------------------------------------------------------------------------------------
+# [4] FAÇA UMA MANOVA USANDO TODOS OS QUATROS TESTES A UM FATOR PARA COMPARAR OS TRÊS MÉTODOS RELATIVAMENTE ÀS VARIÁVEIS ESTUDADAS
+# --------------------------------------------------------------------------------------------------------------------------------
+
+# Criando Modelo de Análise de Variância Multivariada
+manova.model <- manova(cbind(AROMA, SABOR, TEXTURA, UMIDADE) ~ Method, data = df.WIDE)
+
+# Testando a Hipótese Nula
+summary(manova.model, test = "Wilks")
+summary(manova.model, test = "Pillai")
+summary(manova.model, test = "Hotelling-Lawley")
+summary(manova.model, test = "Roy")
+
+# ------------------------------------------------------------------------------------------------------
+# [5] FAÇA ANÁLISE A POSTERIORI SE OS TESTES MULTIVARIADOS REJEITAREM A HIPÓTESE NULA. CONCLUA A ANÁLISE
+# ------------------------------------------------------------------------------------------------------
+
+# AROMA
+emmeans_test(df.WIDE, AROMA ~ Method, p.adjust.method = "bonferroni")
+emmeans_test(df.WIDE, AROMA ~ Method, p.adjust.method = "sidak")
+
+# SABOR
+emmeans_test(df.WIDE, SABOR ~ Method, p.adjust.method = "bonferroni")
+emmeans_test(df.WIDE, SABOR ~ Method, p.adjust.method = "sidak")
+
+# TEXTURA
+emmeans_test(df.WIDE, TEXTURA ~ Method, p.adjust.method = "bonferroni")
+emmeans_test(df.WIDE, TEXTURA ~ Method, p.adjust.method = "sidak")
+
+# UMIDADE
+emmeans_test(df.WIDE, UMIDADE ~ Method, p.adjust.method = "bonferroni")
+emmeans_test(df.WIDE, UMIDADE ~ Method, p.adjust.method = "sidak")
+
+# AROMA
+TukeyHSD(x = aov(AROMA ~ Method, data = df.WIDE), "Method", conf.level = 0.95)
+
+# SABOR
+TukeyHSD(x = aov(SABOR ~ Method, data = df.WIDE), "Method", conf.level = 0.95)
+
+# TEXTURA
+TukeyHSD(x = aov(TEXTURA ~ Method, data = df.WIDE), "Method", conf.level = 0.95)
+
+# UMIDADE
+TukeyHSD(x = aov(UMIDADE ~ Method, data = df.WIDE), "Method", conf.level = 0.95)
